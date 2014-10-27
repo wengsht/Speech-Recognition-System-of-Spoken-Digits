5d29a9f6844e3521b1a7847f414caa1e5d45c92e
liuzhi <qdbs@qq.com>
2014-10-27 22:27:02 +0800

    split

--- src/Feature/KMeanState.h
+++ src/Feature/KMeanState.h
@@ -19,6 +19,14 @@
 #include "HMMState.h"
 #include <vector> 
 #include "Gaussian.h"
+
+struct Cluster{
+	Gaussian * g;
+	std::vector<Feature*> points;
+	double w;
+};
+
+
 class KMeanState : public HMMState {
     public:
         const static std::pair<int, int> NullSeg ;
@@ -32,10 +40,6 @@ class KMeanState : public HMMState {
         void gaussianTrain(int gaussianNum);
         double nodeCost(Feature *inputFeature);
 
-		// lz:老的单高斯版本，用于测试
-        void gaussianTrainTest(int gaussianNum);
-        double nodeCostTest(Feature *inputFeature);
-
         void load(std::stringstream &in, int gaussNum);
         void store(std::stringstream &out);
     private:
@@ -43,39 +47,47 @@ class KMeanState : public HMMState {
         // 注意是和基类的vector<WaveFeatureOP> 一一对应的
         std::vector< std::pair<int, int> > edgePoints;
 		
-		//lz:将该state内所有feature合并到points，
-		//lz:并初始化gaussian参数
-		void initTrain(int gaussianNum);
-		//lz:
-		Feature ** points;
-		//lz: size of points
-		int PointNum;
-		int trainTime;
-		int betterTime;
-		//lz: 
-		void KMeanTrain();
 
-
-		double KMeanNodeCost(Feature * f);
-        void generateInitFeature(Feature &initFeature);
-		//lz
+	
+		//所有的Cluster，一个Cluster一个高斯模型，仅用于split过程
+		//完成后抛弃所有points，将w和g分别存入到真正的权重和模型
+		std::vector<Cluster*> ClusterSet;
 		//真正的模型
 		std::vector<Gaussian*> GaussianModel;
-		//临时的模型
-		std::vector<Gaussian*> GaussianSet;
-		double checkBetter(double);
-		//临时的权重
-		std::vector<double> weight;
 		//真正的权重
 		std::vector<double> w;	
-		//lz: 释放高斯模型空间，清空weight和GaussianSet
-		void clearGaussian();
-		//lz: 计算第pid点最近的高斯模型编号
-		int Point2Clusters(int pid);
+		
+        
 
-        double u[39];
-        double sigma[39];
+
+	private: //方法
+		void clearGaussian();
+		void clearCluster();
+		// 一次训练
+		bool KMeanTrain(int gaussianNum);
+
+		double sumCV();
+		
+		Cluster* findBigCluster();	
+		void deleteCluster(Cluster*c);
+
+		//split 函数
+		bool addTwoCluster(const std::vector<Feature*> & p,double wt);
+
+		
+		Gaussian * initRandGaussian(const std::vector<Feature*>& p)const;
+		
+		void saveGaussianModel();
+
+		int calcFirstCluster();
+
+		void generateInitFeature(Feature &initFeature,
+						const std::vector<Feature*>& points)const;
+		double KMeanNodeCost(Feature * f);
+
 };
+
+
 
 #endif
 
